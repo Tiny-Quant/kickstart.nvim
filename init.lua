cc972691f000be9188d6c65710153d9ff34e7a25
mk85 <mk85@nixos>
2026-03-21 10:10:55 -0500

    Moved lsp and telescope to seperate config files.

--- init.lua
+++ init.lua
@@ -99,6 +99,7 @@ require 'keymaps'
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
+-- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
